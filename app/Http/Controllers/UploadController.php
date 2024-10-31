8bc640de10bf5ccdfe5d9427ef72b2bc2965a1e0
Eduardo Novais <eduardo.novais96@hotmail.com>
2024-10-30 23:18:58 -0300

    [UPDATE] - Tratando erros ao enviar CSV

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Services\Implementation\CsvService;
+use Illuminate\Support\Facades\Validator;
 
 class UploadController extends Controller {
     
@@ -14,8 +15,28 @@ class UploadController extends Controller {
     }
 
     public function processar(Request $request) {
+        $validator = Validator::make($request->all(), $this->regrasImportacao(), $this->mensagensPersonalizadas());
+        
+        if ($validator->fails()) {
+            return new JsonResponse($validator->messages(), 400);
+        }
+
         $this->csvService->uploadAndProcess($request);
 
         return response()->json(['message' => 'Arquivo carregado com sucesso! O processamento será realizado em segundo plano.'], 200);
+    }
+
+    private function regrasImportacao(): array{
+        return [
+            'file' => 'required|file|mimes:csv' // Adicione outros tipos de arquivo se necessário
+        ];
+    }
+
+    private function mensagensPersonalizadas(): array {
+        return [
+            'file.required' => 'Um arquivo é necessário para o upload.',
+            'file.file' => 'O arquivo enviado não é válido.',
+            'file.mimes' => 'O arquivo deve ser do tipo CSV.'
+        ];
     }
 }
